8acb703a55ffda1db3c7151ba8a7f0a050984fec
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-06-27 14:12:05 +0600

    templates loc override

--- eBazar/settings.py
+++ eBazar/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'eBazar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'assets/templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
