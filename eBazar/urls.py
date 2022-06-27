4fe278914a2842083637002385438fc55c669a19
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-06-27 14:09:51 +0600

    added static files url

--- eBazar/urls.py
+++ eBazar/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),
-]
+    
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- product/views.py
+++ product/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Heroku')
+    return render(request, 'core/base.html')
