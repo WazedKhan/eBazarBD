b698da2533879313f683d5edbe846f447b732e7d
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-06-28 20:54:28 +0600

    details views

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from product import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-details'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,11 +1,14 @@
-from django.http import HttpResponse
+from django.views.generic import DetailView
 from django.shortcuts import render
 
 from product.models import Product
 
 def home(request):
     product = {
-        'products':Product.objects.all(),
-        'product': Product.objects.first()
+        'products':Product.objects.all()
     }
     return render(request, 'product/index.html', product)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product/details.html'
