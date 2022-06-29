e6edb62ce68ddf4d8e3df2315667b602621878c4
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-06-29 21:39:16 +0600

    created cart context processor

--- cart/cart.py
+++ cart/cart.py
@@ -1,12 +1,12 @@
 from django.contrib.sessions.models import Session
 from requests import request, session
 
-class Basket():
+class Cart():
 
     def __init__(self, request):
         self.session = request.session
-        basket = self.session.get('s_key')
+        cart = self.session.get('s_key')
         if 's_key' not in request.session:
-            basket = self.session['s_key'] = {}
-        self.basket = basket
-        
+            cart = self.session['s_key'] = {}
+        self.cart = cart
+
