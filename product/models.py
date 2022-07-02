95f3467a047895ff6dd0280ec8cdc7e0b7f9113f
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-07-02 22:33:24 +0600

    reverse relationship

--- product/models.py
+++ product/models.py
@@ -19,7 +19,7 @@ class Category(models.Model):
 
 class SubCategory(models.Model):
     name = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='sub_category')
 
 
     def __str__(self):
