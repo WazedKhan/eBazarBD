5332b42eda4bd276b4365c5bec59d58835c282c6
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-07-03 22:14:28 +0600

    product list view

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,6 @@ from product import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-details'),
+    path('products/list/<int:pk>', views.ProductListView.as_view(), name='list-view'),
+    path('products/list/', views.ProductListView.as_view(), name='list-view'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from django.shortcuts import render
+from product import models
 
 from product.models import Product
 
@@ -17,3 +18,17 @@ class ProductDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["products"] = Product.objects.all()
         return context
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product/list.html'
+    context_object_name = 'products'
+    ordering = ['-created_by']
+
+    def get_queryset(self):
+        if self.kwargs.get('pk'):
+            return models.Product.objects.filter(pk = self.kwargs.get('pk'))
+        else:
+            return models.Product.objects.all()
+
