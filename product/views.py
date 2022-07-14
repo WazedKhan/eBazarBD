bf3b653cf796db3efdec454866aa7d59ba251d41
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-07-14 18:48:49 +0600

    products list

--- product/views.py
+++ product/views.py
@@ -6,7 +6,7 @@ from product.models import Product
 
 def home(request):
     product = {
-        'products':Product.objects.all()
+        'products':Product.objects.all().order_by('-id')[:4]
     }
     return render(request, 'product/index.html', product)
 
