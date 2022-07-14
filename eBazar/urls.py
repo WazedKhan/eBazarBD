f4670e5edf6b4b356e68921fb20fb6eb0e791683
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-07-14 20:57:19 +0600

    order urls

--- eBazar/urls.py
+++ eBazar/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),
     path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls')),
     path('account/', include('user.urls')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
