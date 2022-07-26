31d60b086cb65be4febe8d0b3370fa62ac94d226
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-07-26 23:48:21 +0600

    order attr

--- controllers/views.py
+++ controllers/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.db.models import Count, Q
+
+from orders.models import Order
 
 def home(request):
-    return render(request, 'admin/base.html')
+    order = Order.objects.all()
+    order_pending = Order.objects.filter( billing_status = False)
+    order_confirmed = Order.objects.filter( billing_status = True)
+
+    context = {
+        'orders': order,
+        'orders_pending' : order_pending,
+        'order_confirmed' : order_confirmed
+    }
+    return render(request, 'controllers/base.html', context)
