0dab557f330d73454c844ce4e00dff4131531524
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-07-28 01:35:36 +0600

    order listview

--- controllers/views.py
+++ controllers/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.db.models import Count, Q
+from django.views.generic import ListView
 
 from orders.models import Order
+from product.models import Product
 
 def home(request):
     order = Order.objects.all()
@@ -13,4 +15,15 @@ def home(request):
         'orders_pending' : order_pending,
         'order_confirmed' : order_confirmed
     }
-    return render(request, 'controllers/base.html', context)
+    return render(request, 'controllers/dashboard.html', context)
+
+class OrderListView(ListView):
+    model = Order
+    template_name = 'controllers/orders.html'
+    context_object_name = 'orders'
+    ordering = ['-created_at']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context[""] = 
+        return context
