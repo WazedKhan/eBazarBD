514c7824f7256470b5b5de823949df155a0d32ca
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-07-28 01:35:22 +0600

    order list url

--- controllers/urls.py
+++ controllers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from controllers import views
 
 urlpatterns = [
-    path('', views.home, name='admin-home')
+    path('', views.home, name='admin-home'),
+    path('orders', views.OrderListView.as_view(), name='order-list'),
 ]
