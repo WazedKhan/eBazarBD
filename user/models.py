f853041bc1c98785d48b7769bc5fe6a57ccf79fd
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-08 22:35:55 +0600

    is_sellar

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Sellar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sellar')
+    is_sellar = models.BooleanField(default=False)
+
