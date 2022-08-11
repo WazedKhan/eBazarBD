83ded7f22bd89ea9d4f32561b065d5777e9b9f3c
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-12 00:55:48 +0600

    register form

--- user/views.py
+++ user/views.py
@@ -1,8 +1,7 @@
 from django import forms
+from user.forms import UserRegisterForm
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 
 
 @login_required
@@ -10,14 +9,15 @@ def profile(request):
     return render(request, 'user/index.html')
 
 def registetion(request):
-    if request.user.is_authenticated:
-        return redirect('home')
+    # if request.user.is_authenticated:
+    #     return redirect('home')
 
     if request.POST:
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
+        print(request.POST.get('phone'))
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            print(username)
+            user = form.save()
+            print(user)
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'user/register.html', {'form':form})
