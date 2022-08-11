31a3412fadbb898cf492555d36005e91fef6337d
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-12 00:06:57 +0600

    user register mechanism

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from user import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('register', views.registetion, name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+
 
 @login_required
 def profile(request):
     return render(request, 'user/index.html')
+
+def registetion(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.POST:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            print(username)
+    else:
+        form = UserCreationForm()
+    return render(request, 'user/register.html', {'form':form})
