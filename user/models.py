99627970d1306f107de8a2f97ef251e81557e81e
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-11 21:51:16 +0600

    is_sellar client model

--- user/models.py
+++ user/models.py
@@ -2,7 +2,8 @@ from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 
-class Sellar(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sellar')
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_sellar = models.BooleanField(default=False)
+    phone = models.CharField(max_length=12, unique=True)
 
