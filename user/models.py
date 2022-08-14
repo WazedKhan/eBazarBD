ed5f7867e8bb0b0df845f91c26024d1641b03e0f
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-14 20:48:13 +0600

    rm client model

--- user/models.py
+++ user/models.py
@@ -1,9 +1,1 @@
-from email.policy import default
 from django.db import models
-from django.contrib.auth.models import User
-
-class Client(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_sellar = models.BooleanField(default=False)
-    phone = models.CharField(max_length=12, unique=True)
-
