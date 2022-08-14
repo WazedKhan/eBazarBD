10d3e17f086c04a5648f0d9d3495c0c6d6a43625
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-14 20:49:33 +0600

    rm views and urls

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,4 @@ from django.urls import path
 from user import views
 
 urlpatterns = [
-    path('', views.profile, name='profile'),
-    path('logout', views.logout_view, name='logout'),
-    path('register', views.registetion, name='register'),
 ]
