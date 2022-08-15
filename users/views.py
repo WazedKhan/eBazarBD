50099293466e062919a6462fe68695973b44d4c5
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-16 00:55:36 +0600

    login ops

--- users/views.py
+++ users/views.py
@@ -9,19 +9,19 @@ def profile(request):
     return render(request, 'user/index.html')
 
 def loginView(request):
-    # if request.method == 'POST':
-    #     form = AuthenticationForm(request, data = request.POST)
-    #     if form.is_valid():
-    #         username = form.cleaned_data.get('username')
-    #         password = form.cleaned_data.get('password')
-    #         user = authenticate(username = username, password = password)
-    #         if user is not None:
-    #             login(request, user)
-    #             return redirect('home')
-    #         else:
-    #             messages.error(request, "Invalid username or password.")
-    #     else:
-    #         messages.error(request,"Invalid username or password.")
-    # else:
-    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username = username, password = password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    else:
+        form = AuthenticationForm()
     return render(request, 'user/login.html', {'form':form})
