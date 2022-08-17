d236a15d958026f776ebfb86a170ef4d787b3fe0
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-17 19:50:45 +0600

    sellar models

--- controllers/models.py
+++ controllers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import MyUser
 
-# Create your models here.
+class Sellar(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='user')
+    due = models.FloatField(default=0)
+    last_paid = models.FloatField(default=0)
+    rate = models.FloatField(default=1.0)
+    last_paid_on = models.DateTimeField(auto_now=True)
+    company = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return 'Sellar Info'
