6eddbc7ee2cfba82ef3a11e68ebf91a103852284
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-19 19:44:20 +0600

    seller app

--- accounts/models.py
+++ accounts/models.py
@@ -41,6 +41,7 @@ class User(AbstractBaseUser):
         max_length=255,
         unique=True,
     )
+    is_seller = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
