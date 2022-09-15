4609bb4944e20babb9326843668aa318a09a7abb
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-09-15 21:29:44 +0600

    login/logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
-from .views import index
+from .views import AccountView, logout_view, login_view
 
 app_name = 'Account'
 
 urlpatterns = [
-    path('', index)
+    path('<str:slug>', AccountView.as_view(), name='profile'),
+    path('logging-out', logout_view, name='logout'),
+    path('log-in/', login_view, name='login-view')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.shortcuts import redirect
+from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate, login, logout
+
+from accounts.models import User
 
 # Create your views here.
-def index(request):
-    return JsonResponse({'Status':200})
+class AccountView(LoginRequiredMixin, DetailView):
+    login_url = '/login/'
+    model = User
+    template_name = 'user/index.html'
+
+
+def logout_view(request):
+    logout(request)
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            ...
+        else:
+            # Return an 'invalid login' error message.
+            ...
+    else:
+        return render(request ,'user/login.html')
